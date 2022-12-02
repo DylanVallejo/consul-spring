2fbcae5f5dbf64469abe55edac1c9ea3ac19f7ac
DylanVallejo <dylan1811vallejo@gmail.com>
2022-12-02 17:00:54 -0500

    msvc-curso registry-consul

--- msvc-curso/src/main/java/com/kruger/msvc/curso/MsvcCursoApplication.java
+++ msvc-curso/src/main/java/com/kruger/msvc/curso/MsvcCursoApplication.java
@@ -2,7 +2,9 @@ package com.kruger.msvc.curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsvcCursoApplication {
 
