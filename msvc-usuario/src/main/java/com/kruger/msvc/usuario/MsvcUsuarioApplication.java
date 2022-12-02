810b0a5fb6af3b87936308afbc7f85d24a12db61
Jonathan <jonato96s@gmail.com>
2022-12-02 15:19:14 -0500

    msvc-usuario registry-consul

--- msvc-usuario/src/main/java/com/kruger/msvc/usuario/MsvcUsuarioApplication.java
+++ msvc-usuario/src/main/java/com/kruger/msvc/usuario/MsvcUsuarioApplication.java
@@ -2,8 +2,10 @@ package com.kruger.msvc.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsvcUsuarioApplication {
 
 	public static void main(String[] args) {
